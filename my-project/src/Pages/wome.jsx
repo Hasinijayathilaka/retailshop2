2e144a86d76103c5ff777a4b9e6bac5c7cd4fc80
sashini <sashiniupeksha923@gmail.com>
2025-08-11 17:29:36 +0530

    change

--- my-project/src/Pages/wome.jsx
+++ my-project/src/Pages/wome.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { womenProducts } from '../data/product';
+
 
 const Women = () => {
   const navigate = useNavigate();
